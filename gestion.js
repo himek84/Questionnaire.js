d0854c85f0e0c3abb5f8a2c5037919621f7ea6ba
himek84 <himeko84@live.fr>
2021-05-21 09:19:52 +0200

    gestion.js

--- gestion.js
+++ gestion.js
@@ -3,3 +3,15 @@ let livre1 = {
     titre = "Une journée en formation",
     auteur = "himek carotte"
 };
+
+let livre2 = {
+    reference = "102891",
+    titre = "une soirée en formation",
+    auteur = "himek carotte"
+};
+
+let livre3 = {
+    reference = "102890",
+    titre = "Une semaine en formation",
+    auteur = "himek carotte"
+};
